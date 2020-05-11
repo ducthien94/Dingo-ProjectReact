5febb42f549fc0d579cf0ce9db0ec2200f4bb7e1
Van Duc <vanduc2314@gmail.com>
2020-05-11 20:37:29 +0700

    update firebase api key

--- firebase.js
+++ firebase.js
@@ -2,15 +2,15 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/database"
   // Your web app's Firebase configuration
-  const config= {
-    apiKey: "AIzaSyAnfD9rwyDfT7FToySvYxukautWWJ-Y36k",
+  const config = {
+    apiKey: "AIzaSyBd9UvPGFOFfHrmtNfFwn6TRRaN1EohAFk",
     authDomain: "dingo-2314.firebaseapp.com",
     databaseURL: "https://dingo-2314.firebaseio.com",
     projectId: "dingo-2314",
     storageBucket: "dingo-2314.appspot.com",
     messagingSenderId: "200666974505",
     appId: "1:200666974505:web:25366a102014e15e4f0f38",
-    measurementId: "G-M94QPKFNN2"
+    measurementId: "G-M94QPKFNN2",
   };
   // Initialize Firebase
   if (!firebase.apps.length) {
